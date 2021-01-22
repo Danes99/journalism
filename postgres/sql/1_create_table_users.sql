7c7c9542b151296c7b3e087228f1735db551f7d3
Clement <clement.stauner@gmail.com>
2021-01-22 16:58:41 +0100

    Changed utc dates

--- postgres/sql/1_create_table_users.sql
+++ postgres/sql/1_create_table_users.sql
@@ -7,7 +7,7 @@ CREATE TABLE users(
     url VARCHAR(64) NOT NULL UNIQUE,
     email VARCHAR(64) NOT NULL UNIQUE,
 	password VARCHAR(150),
-    created_at timestamp with time zone NOT NULL DEFAULT ( NOW() AT TIME ZONE 'UTC+2' )
+    created_at timestamp with time zone NOT NULL DEFAULT ( NOW() AT TIME ZONE 'UTC+1' )
 );
 
 GRANT ALL PRIVILEGES ON TABLE users TO postgres;
--- postgres/sql/2_create_table_jwt.sql
+++ postgres/sql/2_create_table_jwt.sql
@@ -5,7 +5,7 @@ CREATE TABLE jwt(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id INT NOT NULL,
     token VARCHAR(200),
-    created_at timestamp with time zone NOT NULL DEFAULT ( NOW() AT TIME ZONE 'UTC+2' ),
+    created_at timestamp with time zone NOT NULL DEFAULT ( NOW() AT TIME ZONE 'UTC+1' ),
     CONSTRAINT fk_user
         FOREIGN KEY(user_id) 
 	    REFERENCES users(id)
