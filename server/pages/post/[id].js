5921b2151c567df40710903610304927e920b1f4
Clement <clement.stauner@protonmail.com>
2021-01-26 00:25:14 +0100

    Created first article

--- server/pages/post/[id].js
+++ server/pages/post/[id].js
@@ -1,14 +1,35 @@
 import Head from 'next/head'
 
-export default function Post() {
+import Date from '../../components/date'
+
+import DAO_BASE_URL from '../../config/DAO_BASE_URL'
+
+export async function getServerSideProps() {
+    const id = '1'
+    const req = await fetch(DAO_BASE_URL + 'article/' + id)
+    const article = await req.json()
+
+    return { props: { article } }
+}
+
+export default function Post({ article }) {
+
     return (
         <div>
 
             <Head>
-                <title>Title</title>
+                <title>{article.title}</title>
             </Head>
             <article>
-                <h1>Test</h1>
+                <h1>Content</h1>
+
+                <h2>
+                    Updated <Date dateString={article.updated_at} />
+                </h2>
+
+                <p>
+                    {article.content}
+                </p>
                 
             </article>
         </div>
