d50e9ef4ef7aff496c7d684f1c24b45d3df5e148
Clement <clement.stauner@protonmail.com>
2021-02-04 00:21:46 +0100

    Added dynamic article fetching

--- server/pages/post/[id].js
+++ server/pages/post/[id].js
@@ -4,9 +4,10 @@ import Date from '../../components/date'
 
 import DAO_BASE_URL from '../../config/DAO_BASE_URL'
 
-export async function getServerSideProps() {
-    const id = '1'
-    const req = await fetch(DAO_BASE_URL + 'article/' + id)
+export async function getServerSideProps({ params }) {
+
+    // Get the article from the DAO
+    const req = await fetch(DAO_BASE_URL + 'article/' + params.id)
     const article = await req.json()
 
     return { props: { article } }
@@ -20,13 +21,15 @@ export default function Post({ article }) {
             <Head>
                 <title>{article.title}</title>
             </Head>
+
             <article>
-                <h1>Content</h1>
 
                 <h2>
                     Updated <Date dateString={article.updated_at} />
                 </h2>
 
+
+                <h1>Content</h1>
                 <p>
                     {article.content}
                 </p>
