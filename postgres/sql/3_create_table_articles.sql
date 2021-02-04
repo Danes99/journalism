8a8e7a95136c16e7b3055596b5d65963401b808c
Clement <clement.stauner@protonmail.com>
2021-02-04 17:18:14 +0100

    Added trigger updated_at for articles table

--- postgres/sql/3_create_table_articles.sql
+++ postgres/sql/3_create_table_articles.sql
@@ -11,4 +11,20 @@ CREATE TABLE articles (
     created_at timestamp with time zone NOT NULL DEFAULT ( NOW() AT TIME ZONE 'UTC-1' )
 );
 
+-- Create a Trigger function
+-- to update "updated_at" when UPDATE the table "articles"
+-- https://www.dbrnd.com/2016/03/postgresql-update-the-timestamp-column-with-the-use-of-trigger/
+CREATE OR REPLACE FUNCTION trg_fn_articles_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = NOW() AT TIME ZONE 'UTC-1'; 
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- Create an UPDATE TRIGGER
+CREATE TRIGGER trg_update_articles BEFORE UPDATE
+ON articles FOR EACH ROW EXECUTE PROCEDURE 
+trg_fn_articles_updated_at();
+
 GRANT ALL PRIVILEGES ON TABLE articles TO postgres;
